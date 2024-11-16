ffadd92c5bd7bccdbf64da6f6cdd05c36b75b2e5
Ankit Tangariya <ankittangariya@gmail.com>
2024-11-16 22:22:38 +0530

    3rd upload

---
+++ variablesandconstants.js
@@ -0,0 +1,7 @@
+const accountNumber = 122434;
+let accountEmail = "honor90@gmail.com";
+var accountPassword = "124245"; //don't use this as this creates problem in '{}' loops and statements
+accountCity = "delhi"
+let accountState;    //creates a variable with undefined value
+console.table([accountNumber,accountEmail,accountPassword,accountCity,accountState]);
+//console.table([var1,var2,var3,var4]) gives the data in tabular form with respect to their index
